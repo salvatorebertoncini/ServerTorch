83e8035f76926b33984aeee3eae760946843dc5b
Salvo Bertoncini <salvobertoncini@users.noreply.github.com>
2017-07-15 11:55:51 +0200

    parse json from request and build the response with foo bar example

--- Server/views.py
+++ Server/views.py
@@ -2,7 +2,12 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
+@csrf_exempt
 def index(request):
-    return HttpResponse("Cia1!")
+    if request.method == "POST":
+        data = json.loads(request.body)
+        return HttpResponse("foo: %s, " % str(data["foo"]) + "bar: %s" % str(data["bar"]))
