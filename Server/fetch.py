4200f61ce29ea0efe30cbd5762cdd5b7324d1212
Salvo Bertoncini <salvobertoncini@users.noreply.github.com>
2017-07-16 21:05:28 +0200

    fix fetchData example

--- Server/fetch.py
+++ Server/fetch.py
@@ -3,9 +3,32 @@ import datetime
 from database import *
 from logs import saveLog
 
+#JSON Example
+#{ "date": "", "stats": {"BuildInfo": {"Manufacturer": [], "ManufacturerCounter": 0 }, "TelephoneInfo":  { "IMEIcounter": 0, "IMEIcollected": []} },"flag": true }
+
+
+def IMEIFetch(imei, imeiList, imeiCounter):
+    if imei not in imeiList:
+        imeiCounter += 1
+        imeiList.append(imei)
+    return imeiList, imeiCounter
+
+
+def ManufacturerFetch(device, deviceList, deviceCounter):
+    if device not in deviceList:
+        deviceList.append(device)
+        deviceCounter += 1
+    return deviceList, deviceCounter
+
+
+def DevicePercentage(device, deviceList, deviceCounter):
+    if device in deviceList:
+        return 1 / deviceCounter
+    else:
+        return 0
+
 
 def fetchData(data):
-
     #MongoDB connection, selecting "datafetching" database
     client = connectMongoDB()
     collection = selectCollectionMongoDB(client, "datafetching")
@@ -13,25 +36,22 @@ def fetchData(data):
     #Select latest element
     latest = selectLatestNElementsMongoDB(collection, 1)
 
-    #Fetch element example
+    #Fetch IMEI
     imeiList = latest[0]["stats"]["TelephoneInfo"]["IMEIcollected"]
+    imeiCounter = latest[0]["stats"]["TelephoneInfo"]["IMEIcounter"]
+    imeiList, imeiCounter = IMEIFetch(data["TelephoneInfo"]["IMEI"], imeiList, imeiCounter)
 
-    #if IMEI is not in db list
-    if data["TelephoneInfo"]["IMEI"] not in imeiList :
-        imeiCounter = latest[0]["stats"]["TelephoneInfo"]["IMEIcounter"]
-        imeiCounter += 1
-        imeiList.append(data["TelephoneInfo"]["IMEI"])
 
-    #if Manufacturer is not in db list
+    #Fetch Manufacturer
     deviceList = latest[0]["stats"]["BuildInfo"]["Manufacturer"]
-    if data["BuildInfo"]["Manufacturer"] not in deviceList:
-        deviceList.append(data["BuildInfo"]["Manufacturer"])
-        deviceCounter = latest[0]["stats"]["BuildInfo"]["ManufacturerCounter"]
-        deviceCounter += 1
+    deviceCounter = latest[0]["stats"]["BuildInfo"]["ManufacturerCounter"]
+    deviceList, deviceCounter = ManufacturerFetch(data["BuildInfo"]["Manufacturer"], deviceList, deviceCounter)
 
     #e.g. percentage of Samsung devices in the database
-    if "Samsung" in deviceList:
-        samsung = 1 / deviceCounter
+    samsung = DevicePercentage("Samsung", deviceList, deviceCounter)
+
+    #e.g. percentage of Apple devices in the database, LOL
+    apple = DevicePercentage("Apple Inc.", deviceList, deviceCounter)
 
     """   
     #LambdaFetch with map function
@@ -44,7 +64,9 @@ def fetchData(data):
 
     #Save fetched element
     result = insertElementMongoDB(collection, latest)
-    saveLog('fetch.py','Successfully inserted with ID: {0}'.format(result.inserted_id)+'\n'+ "Percentuale dispositivi Samsung: %s" % str(samsung))
+    saveLog('fetch.py','Successfully inserted with ID: {0}'.format(result.inserted_id)+'\n')
+    saveLog('fetch.py', "Percentuale dispositivi Samsung: %s" % str(samsung))
+    saveLog('fetch.py', "Percentuale dispositivi Apple: %s" % str(apple))
 
     #Closing connection
     closeMongoDB(client)
--- logs.md
+++ logs.md
@@ -7,3 +7,48 @@ Percentuale dispositivi Samsung: 0.333333333333
  
 **[2017-07-16 20:33:52.757000]:** Successfully inserted, with ID: 596bb190198adf0aa4be5116
  
+## fetch.py
+ 
+**[2017-07-16 20:49:00.306000]:** Successfully inserted with ID: 596bb51c198adf0aa4be5119
+Percentuale dispositivi Samsung: 0.25
+ 
+## request.py
+ 
+**[2017-07-16 20:49:00.346000]:** Successfully inserted, with ID: 596bb51c198adf0aa4be511a
+ 
+## fetch.py
+ 
+**[2017-07-16 21:00:58.602000]:** Successfully inserted with ID: 596bb7ea198adf190c9da0af
+Percentuale dispositivi Samsung: 0.2
+ 
+## request.py
+ 
+**[2017-07-16 21:00:58.628000]:** Successfully inserted, with ID: 596bb7ea198adf190c9da0b0
+ 
+## fetch.py
+ 
+**[2017-07-16 21:03:06.774000]:** Successfully inserted with ID: 596bb86a198adf222450852b
+
+ 
+## fetch.py
+ 
+**[2017-07-16 21:03:33.010000]:** Successfully inserted with ID: 596bb885198adf20908fdec8
+
+ 
+## fetch.py
+ 
+**[2017-07-16 21:04:38.600000]:** Successfully inserted with ID: 596bb8c6198adf2608a6a744
+
+ 
+## fetch.py
+ 
+**[2017-07-16 21:04:38.605000]:** Percentuale dispositivi Samsung: 0.166666666667
+ 
+## fetch.py
+ 
+**[2017-07-16 21:04:38.609000]:** Percentuale dispositivi Apple: 0
+ 
+## request.py
+ 
+**[2017-07-16 21:04:38.639000]:** Successfully inserted, with ID: 596bb8c6198adf2608a6a745
+ 
